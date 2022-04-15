64c35361077a61e9050be7266a97259ce287e885
liza-kl <hello@li54.dev>
2022-04-15 07:55:35 +0200

    :sparkles: Added basic Main Class code

--- src/main/simplepoker/Main.java
+++ src/main/simplepoker/Main.java
@@ -1,7 +1,29 @@
 package simplepoker;
 
-public class Main {
-    public static void main(String[] args) {
+import simplepoker.enums.CardValue;
 
+import java.util.List;
+
+import static simplepoker.enums.CardSuit.*;
+import static simplepoker.enums.CardSuit.S;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Card> firstPokerHand = List.of(
+                new Card(H, CardValue.THREE),
+                new Card(C, CardValue.THREE),
+                new Card(S, CardValue.NINE),
+                new Card(S, CardValue.NINE),
+                new Card(H, CardValue.K));
+        List<Card> secondPokerHand = List.of(
+                new Card(D, CardValue.THREE),
+                new Card(S, CardValue.THREE),
+                new Card(D, CardValue.EIGHT),
+                new Card(H, CardValue.EIGHT),
+                new Card(S, CardValue.A));
+
+        Integer winner = PokerHandService.computeWinner(firstPokerHand, secondPokerHand);
+        System.out.println("The winner is PokerHand No." + winner);
     }
 }
