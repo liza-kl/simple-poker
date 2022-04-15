8bd69a5f1a56b777722753d9815db8f7dde1346e
liza-kl <hello@li54.dev>
2022-04-15 07:53:41 +0200

    :white_check_mark: Renamed the tests – more to come

--- src/test/simplepoker/PokerHandServiceTest.java
+++ src/test/simplepoker/PokerHandServiceTest.java
@@ -1,38 +1,19 @@
 package simplepoker;
 
 import org.junit.jupiter.api.Test;
+import simplepoker.enums.CardValue;
+import simplepoker.winnerstrategy.PairStrategy;
+import simplepoker.winnerstrategy.TwoPairsStrategy;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static simplepoker.CardSuit.*;
+import static simplepoker.enums.CardSuit.*;
 
 class PokerHandServiceTest {
 
     @Test
-    void getPokerHandValues() {
-        List<Card> pokerHand = List.of(new Card(S, CardValue.A),
-                new Card(S, CardValue.TEN),
-                new Card(C, CardValue.TEN),
-                new Card(D, CardValue.TEN),
-                new Card(S, CardValue.FIVE));
-        PokerHandService pokerHandService = new PokerHandService();
-        //assertTrue(new Flush().isSatisfiedBy(pokerHand));
-        assertEquals(pokerHandService.getPokerHandRank(pokerHand), "Flush");
-    }
-
-    @Test
-    void consecutivePokerHand() {
-        List<Card> pokerHand = List.of(new Card(S, CardValue.SIX),
-                new Card(S, CardValue.NINE),
-                new Card(S, CardValue.FIVE),
-                new Card(D, CardValue.EIGHT),
-                new Card(S, CardValue.SEVEN));
-        PokerHandService pokerHandService = new PokerHandService();
-    }
-
-    @Test
-    void calculateRegularWinner() {
+    void calculateWinnerByValueIfNoOneHasAPokerHand() {
         List<Card> firstPokerHand = List.of(new Card(H, CardValue.TWO),
                 new Card(C, CardValue.THREE),
                 new Card(S, CardValue.FOUR),
@@ -43,10 +24,11 @@ class PokerHandServiceTest {
                 new Card(D, CardValue.SEVEN),
                 new Card(H, CardValue.J),
                 new Card(S, CardValue.K));
-        assertEquals(PokerHandService.computeWinner(firstPokerHand, secondPokerHand), 2);
+        assertEquals(2, PokerHandService.computeWinner(firstPokerHand, secondPokerHand));
     }
+
     @Test
-    void calculateHighestCardWinner() {
+    void calculateIfBothHaveHighestCardWinner() {
         List<Card> firstPokerHand = List.of(new Card(H, CardValue.TWO),
                 new Card(C, CardValue.THREE),
                 new Card(S, CardValue.SEVEN),
@@ -57,8 +39,38 @@ class PokerHandServiceTest {
                 new Card(D, CardValue.SEVEN),
                 new Card(H, CardValue.J),
                 new Card(S, CardValue.K));
-        assertEquals(PokerHandService.computeWinner(firstPokerHand, secondPokerHand), 1);
+        assertEquals(1, PokerHandService.computeWinner(firstPokerHand, secondPokerHand));
     }
 
+    @Test
+    void calculateIfBothHavePairWinner() {
+        List<Card> firstPokerHand = List.of(new Card(H, CardValue.THREE),
+                new Card(C, CardValue.THREE),
+                new Card(S, CardValue.SEVEN),
+                new Card(S, CardValue.Q),
+                new Card(H, CardValue.K));
+        List<Card> secondPokerHand = List.of(new Card(D, CardValue.THREE),
+                new Card(S, CardValue.THREE),
+                new Card(D, CardValue.SEVEN),
+                new Card(H, CardValue.J),
+                new Card(S, CardValue.A));
+        PairStrategy pairStrategy = new PairStrategy();
+        assertEquals(2, pairStrategy.computeWinner(firstPokerHand, secondPokerHand));
+    }
 
+    @Test
+    void calculateIfBothHaveTwoPairsWinner() {
+        List<Card> firstPokerHand = List.of(new Card(H, CardValue.THREE),
+                new Card(C, CardValue.THREE),
+                new Card(S, CardValue.NINE),
+                new Card(S, CardValue.NINE),
+                new Card(H, CardValue.K));
+        List<Card> secondPokerHand = List.of(new Card(D, CardValue.THREE),
+                new Card(S, CardValue.THREE),
+                new Card(D, CardValue.EIGHT),
+                new Card(H, CardValue.EIGHT),
+                new Card(S, CardValue.A));
+        TwoPairsStrategy twoPairsStrategy = new TwoPairsStrategy();
+        System.out.println(twoPairsStrategy.computeWinner(firstPokerHand,secondPokerHand));
+    }
 }
